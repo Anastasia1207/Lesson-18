023089d3ffac5cd7d1a8744dc16fcb2313ae9c76
Anastasija <78434027+Anastasia1207@users.noreply.github.com>
2021-03-16 18:35:27 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+console.log('Task-1')
+
+let number2 = 0;
+while (number2 <= 100) {
+      if (number2 % 2 === 0  && number2 % 3 === 0)  {
+        console.log(number2, 'Fizz Bazz');
+    } else if (number2 % 2 === 0 ) {
+        console.log(number2, 'Fizz');
+    } else if (number2 % 3 === 0 ) {
+        console.log(number2, 'Bazz');
+    } else {
+    console.log(number2);
+    }
+    number2++; 
+}
+
+console.log('Task-2'); 
+
+for ( i = 1000; i > 49; i -=7) {
+    console.log('the number is', i)
+ if (i % 2 === 0)  {
+  console.log(i, 'the number is Even');
+     } else {
+        console.log(i, 'the number is Odd');
+     }
+    }
+    
